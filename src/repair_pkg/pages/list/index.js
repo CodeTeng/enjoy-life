486afd213147fa383cfb2dc3be2452bdc54c1af9
teng <teng2002823@outlook.com>
2024-01-24 21:10:49 +0800

    完成报修管理列表页面渲染

--- src/repair_pkg/pages/list/index.js
+++ src/repair_pkg/pages/list/index.js
@@ -1,12 +1,28 @@
 Page({
-  goDetail() {
+  data: {
+    repairList: [],
+    isFirstLoading: true
+  },
+
+  onShow() {
+    this.getRepairList()
+  },
+
+  async getRepairList() {
+    const res = await wx.http.get('/repair', { current: 1, pageSize: 10 })
+    this.setData({
+      repairList: res.data.rows,
+      isFirstLoading: false
+    })
+  },
+  goDetail(ev) {
     wx.navigateTo({
-      url: '/repair_pkg/pages/detail/index',
+      url: '/repair_pkg/pages/detail/index?id=' + ev.mark.id
     })
   },
   addRepair() {
     wx.navigateTo({
-      url: '/repair_pkg/pages/form/index',
+      url: '/repair_pkg/pages/form/index'
     })
-  },
+  }
 })
