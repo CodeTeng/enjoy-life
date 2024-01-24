9ed16161022136457c53e1f959932f520c16bf6d
teng <teng2002823@outlook.com>
2024-01-24 21:10:36 +0800

    完成报修管理详情功能

--- src/repair_pkg/pages/detail/index.js
+++ src/repair_pkg/pages/detail/index.js
@@ -1,7 +1,35 @@
 // map.js
 Page({
   data: {
+    repairDetail: {},
     latitude: 40.060539,
     longitude: 116.343847,
   },
+  onLoad({ id }) {
+    this.getRepairDetail(id)
+    this.id = id
+  },
+  async getRepairDetail(id) {
+    const res = await wx.http.get(`/repair/${id}`)
+    this.setData({
+      repairDetail: res.data
+    })
+  },
+  editRepair() {
+    wx.navigateTo({
+      url: '/repair_pkg/pages/form/index?id=' + this.id,
+    })
+  },
+  async cancelRepair() {
+    const { confirm } = await wx.showModal({
+      content: '确定取消报修吗？',
+    })
+    if (confirm) {
+      const id = this.id
+      await wx.http.put(`/cancel/repaire/${id}`)
+      wx.navigateTo({
+        url: '/repair_pkg/pages/list/index',
+      })
+    }
+  }
 })
